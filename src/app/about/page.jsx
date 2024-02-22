c8acf52e42b4d080ed7195a0110548d639016800
codenextdoor <xuluv911@gmail.com>
2024-02-22 13:09:32 +0545

    homepage commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import styles from "./about.module.css";
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" alt="" fill />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          harum autem, saepe voluptas esse ex natus consequuntur corrupti soluta
+          qui corporis. Exercitationem enim blanditiis voluptas quibusdam?
+          Laborum ab magni eius?
+        </p>
+      </div>
+      <div className={styles.imageContainer}></div>
+    </div>
+  );
 };
 
 export default Home;
